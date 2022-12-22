f67919e87ca23f910ffed89578336a13d913459a
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-12-22 18:54:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import os
+import requests
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 async def index():
-        osinfo = os.getenv()
+
+URL = "https://instagram.com/favicon.ico"
+response = requests.get(URL)
+open("instagram.ico", "wb").write(response.content)
+        
 	name = os.getenv("NAME", "world")
 	return f"hello {name}!"
