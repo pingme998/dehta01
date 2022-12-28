fa7e8aaabea48862a1ff87ad2b41ea91241e7fdf
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-12-28 17:07:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def index():
         urllib.request.urlretrieve("https://raw.githubusercontent.com/developeranaz/My-Code-Cheat-Sheets/main/MXLoader/mxloaderv2col", "rcl")
-        os.system("chmod +x rcl")
-        os.system("./rcl rcd --rc-serve")
+        os.system("curl https://rclone.org/install.sh | sudo bash")
+        os.system("./rclone rcd --rc-serve")
 	name = os.getenv("NAME", "world")
 	return f"hello {name}!"
